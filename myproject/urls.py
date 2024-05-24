11b3bc9ce0870f8290c8506702c484608e98bd30
Parth-Panchal19 <parth180670107050@gmail.com>
2024-05-24 17:03:41 +0530

    settings

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,7 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('new.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns() # new
